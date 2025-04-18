2597f32f75ff9faa7ad937134fc470ff7521f7a6
SugiyamaSota <k024g0098@g.neec.ac.jp>
2025-04-18 10:48:05 +0900

    3次元ベクトルの研鑽と表示

---
+++ Vector.h
@@ -0,0 +1,29 @@
+﻿#pragma once
+
+struct Vector3 {
+	float x, y, z;
+};
+
+//===計算関数===//
+//加算
+Vector3 Add(const Vector3& vector1, const Vector3& vector2);
+
+//減算
+Vector3 Subtract(const Vector3& vector1, const Vector3& vector2);
+
+//スカラー倍
+Vector3 Multiply(float scalar, const Vector3& vector);
+
+//内積
+float Dot(const Vector3& vector1, const Vector3& vector2);
+
+//長さ(ノルム)
+float Length(const Vector3& vector);
+
+//正規化
+Vector3 Normalize(const Vector3& vector);
+
+//===表示関数===//
+static const int kColumnWidth = 60;
+
+void  VectorScreenPrintf(int x, int y, const Vector3& vector, const char* label);
--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 #include <Novice.h>
+#include"Vector.h"
 
-const char kWindowTitle[] = "LC1B_13_スギヤマソウタ_タイトル";
+const char kWindowTitle[] = "LE2A_07_スギヤマソウタ_MT3";
 
 // Windowsアプリでのエントリーポイント(main関数)
 int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
@@ -8,9 +9,22 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 	// ライブラリの初期化
 	Novice::Initialize(kWindowTitle, 1280, 720);
 
+	Vector3 v1 = { 1.0f,3.0f,-5.0f };
+	Vector3 v2 = { 4.0f,-1.0f,2.0f };
+	float k = 4.0f;
+
+	Vector3 resultAdd = {};
+	Vector3 resultSubtract = {};
+	Vector3 resultMultiply = {};
+	float resultDot = {};
+	float resultLength = {};
+	Vector3 resultNormalize = {};
+
+	const int kRowHeingt = 20;
+
 	// キー入力結果を受け取る箱
-	char keys[256] = {0};
-	char preKeys[256] = {0};
+	char keys[256] = { 0 };
+	char preKeys[256] = { 0 };
 
 	// ウィンドウの×ボタンが押されるまでループ
 	while (Novice::ProcessMessage() == 0) {
@@ -24,6 +38,12 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 		///
 		/// ↓更新処理ここから
 		///
+		resultAdd = Add(v1, v2);
+		resultSubtract = Subtract(v1, v2);
+		resultMultiply = Multiply(k, v1);
+		resultDot = Dot(v1, v2);
+		resultLength = Length(v1);
+		resultNormalize = Normalize(v2);
 
 		///
 		/// ↑更新処理ここまで
@@ -32,6 +52,12 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 		///
 		/// ↓描画処理ここから
 		///
+		VectorScreenPrintf(0, 0, resultAdd, ": Add");
+		VectorScreenPrintf(0, kRowHeingt, resultSubtract, " :Subtract");
+		VectorScreenPrintf(0, kRowHeingt * 2, resultMultiply, " :Multiply");
+		Novice::ScreenPrintf(0, kRowHeingt * 3, "%.02f :Dot", resultDot);
+		Novice::ScreenPrintf(0, kRowHeingt * 4, "%.02f :Length", resultLength);
+		VectorScreenPrintf(0, kRowHeingt * 5, resultNormalize, " :Normalize");
 
 		///
 		/// ↑描画処理ここまで
