957dce88ee46c8a38cb8e837c9a4e3c4a7c3e064
SugiyamaSota <k024g0098@g.neec.ac.jp>
2025-04-18 11:12:02 +0900

    拡縮と平行移動の行列を作成

---
+++ Convert.h
@@ -0,0 +1,26 @@
+﻿#pragma once
+#include"Vector.h"
+#include"Matrix.h"
+#include<assert.h>
+
+/// <summary>
+/// 平行移動行列
+/// </summary>
+/// <param name="translate">移動量</param>
+/// <returns>平行移動後の行列</returns>
+Matrix4x4 MakeTranslateMatrix(const Vector3& translate);
+
+/// <summary>
+/// 拡縮後の行列
+/// </summary>
+/// <param name="scale">拡縮の大きさ</param>
+/// <returns>拡縮後の行列</returns>
+Matrix4x4 MakeScaleMatrix(const Vector3& scale);
+
+/// <summary>
+/// 座標変換
+/// </summary>
+/// <param name="vector">変換させる点</param>
+/// <param name="matrix">変換する大きさ</param>
+/// <returns>変換後の座標</returns>
+Vector3 Transform(const Vector3& vector, const Matrix4x4& matrix);
--- main.cpp
+++ main.cpp
@@ -1,6 +1,5 @@
 #include <Novice.h>
-#include"Vector.h"
-#include"Matrix.h"
+#include"Convert.h"
 
 const char kWindowTitle[] = "LC1B_13_スギヤマソウタ_タイトル";
 
@@ -10,28 +9,20 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 	// ライブラリの初期化
 	Novice::Initialize(kWindowTitle, 1280, 720);
 
-	Matrix4x4 m1 = { 3.2f,0.7f,9.6f,4.4f,
-					 5.5f,1.3f,7.8f,2.1f,
-					 6.9f,8.0f,2.6f,1.0f,
-					 0.5f,7.2f,5.1f,3.3f };
-
-	Matrix4x4 m2 = { 4.1f,6.5f,3.3f,2.2f,
-					 8.8f,0.6f,9.9f,7.7f,
-					 1.1f,5.5f,6.6f,0.0f,
-					 3.3f,9.9f,8.8f,2.2f };
-
-	Matrix4x4 resultAdd = Add(m1, m2);
-	Matrix4x4 resultSubtract = Subtract(m1, m2);
-	Matrix4x4 resultMultiply = Multiply(m1, m2);
-	Matrix4x4 resultInverseM1 = Inverse(m1);
-	Matrix4x4 resultInverseM2 = Inverse(m2);
-	Matrix4x4 resultTransposeM1 = Transpose(m1);
-	Matrix4x4 resultTransposeM2 = Transpose(m2);
-	Matrix4x4 resultIdentity = MakeIdentity4x4();
-
-
 	const int kRowHeight = 20;
-	const int kColumnWidth = 50;
+
+	Vector3 translate = { 4.1f,2.6f,0.8f };
+	Vector3 scale = { 1.5f,5.2f,7.3f };
+	Matrix4x4 translateMatrix = MakeTranslateMatrix(translate);
+	Matrix4x4 scaleMatrix = MakeScaleMatrix(scale);
+	Vector3 point = { 2.3f,3.8f,1.4f };
+	Matrix4x4 transformMatrix = {
+		1.0f,2.0f,3.0f,4.0f,
+		3.0f,1.0f,1.0f,2.0f,
+		1.0f,4.0f,2.0f,3.0f,
+		2.0f,2.0f,1.0f,3.0f
+	};
+	Vector3 transformed = Transform(point, transformMatrix);
 
 	// キー入力結果を受け取る箱
 	char keys[256] = { 0 };
@@ -49,14 +40,7 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 		///
 		/// ↓更新処理ここから
 		///
-		MatrixScreenPrintf(0, 0, resultAdd, "Add");
-		MatrixScreenPrintf(0, kRowHeight * 5, resultSubtract, "Subtract");
-		MatrixScreenPrintf(0, kRowHeight * 5 * 2, resultMultiply, "Multiply");
-		MatrixScreenPrintf(0, kRowHeight * 5 * 3, resultInverseM1, "InverseM1");
-		MatrixScreenPrintf(0, kRowHeight * 5 * 4, resultInverseM2, "InverseM2");
-		MatrixScreenPrintf(kColumnWidth * 5, 0, resultTransposeM1, "TransposeM1");
-		MatrixScreenPrintf(kColumnWidth * 5, kRowHeight * 5, resultTransposeM2, "TransposeM2");
-		MatrixScreenPrintf(kColumnWidth * 5, kRowHeight * 5 * 2, resultIdentity, "Identity");
+
 		///
 		/// ↑更新処理ここまで
 		///
@@ -64,7 +48,9 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 		///
 		/// ↓描画処理ここから
 		///
-
+		VectorScreenPrintf(0, 0, transformed, "transformed");
+		MatrixScreenPrintf(0, kRowHeight, translateMatrix, "translateMatrix");
+		MatrixScreenPrintf(0, kRowHeight * 6, scaleMatrix, "scaleMatrix");
 		///
 		/// ↑描画処理ここまで
 		///
