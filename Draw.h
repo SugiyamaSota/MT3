0c9ac3973931c03e07a6678844e6d2d90b87decb
SugiyamaSota <k024g0098@g.neec.ac.jp>
2025-07-01 17:59:21 +0900

    03_00完了

--- Draw.h
+++ Draw.h
@@ -1,4 +1,5 @@
 ﻿#pragma once
+#include<cstdint>
 
 ///// 前方宣言 /////
 class Camera;
@@ -6,18 +7,22 @@ struct Sphere;
 struct Plane;
 struct Triangle;
 struct AABB;
+struct Vector3;
 
 //グリッドの描画
 void DrawGrid(Camera* camera);
 
 //球体の描画
-void DrawSphere(const Sphere& sphere, Camera* camera, int color);
+void DrawSphere(const Sphere& sphere, Camera* camera, uint32_t color);
 
 //平面の描画
-void DrawPlane(const Plane& plane, Camera* camera, int color);
+void DrawPlane(const Plane& plane, Camera* camera, uint32_t color);
 
 //三角形の描画
-void DrawTriangle(const Triangle& triangle, Camera* camera, int color);
+void DrawTriangle(const Triangle& triangle, Camera* camera, uint32_t color);
 
 //AABBの描画
-void DrawAABB(const AABB& aabb, Camera* camera, int color);
+void DrawAABB(const AABB& aabb, Camera* camera, uint32_t color);
+
+void DrawBezier(const Vector3& controlPosint0, const Vector3& controlPosint1, const Vector3& controlPosint2,
+	Camera* camera, uint32_t color);
